0a6714159aa9f1be87c67732ef4b7e02d36fd1fd
heardman <markyheard@gmail.com>
2021-02-14 18:12:57 -0800

    added a logo for the header and add som accompanying css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import Header from "./components/Header";
-import SignInButton from "./components/SignInButton";
-import SignOutButton from "./components/SignOutButton";
-import Profile from "./components/Profile";
 import './App.css';
 
 
 function App() {
   return (
-    <div className="">
-      {/* <Header/> */}
-      <SignInButton/>
-      <SignOutButton/>
-      <Profile/>
+    <div id='app' className='dark'>
+      <Header/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,9 @@
+import logo from "../text-logo.svg"
 const Header = () => {
     return (
         <header>
-            <svg xmlns="http://www.w3.org/2000/svg" width="228" height="154" viewBox="0 0 228 154">
-                <text id="HeaderTitle" 
-                      transform="translate(2 61)" 
-                      fill="rgba(112,112,112,0)" 
-                      stroke="#dadfe1" stroke-width="2" 
-                      font-size="64" 
-                      font-family="Roboto-Bold, Roboto" 
-                      font-weight="700">
-                    <tspan x="0" y="0">fitTrack</tspan>
-                    <tspan x="0" y="75"></tspan>
-                    </text>
-            </svg>
-        </header>
+           <img id='header-logo' src={logo} alt='a light grey outline of the word "fitTrack"'></img>
+        </header >
     );
 }
 export default Header;
