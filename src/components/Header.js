08a17b7482a509d0c660380b4dba31914d44fe73
heardman <markyheard@gmail.com>
2021-02-14 19:15:43 -0800

    added documentation for header and css currently in application

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
+/**
+ * @name Header
+ * @author Mark Heard
+ * @version 1.0
+ * @copyright 2021
+ * @component A simple header comoponent used to display a title logo
+ */
+
+// import and create a reference to the text-logo file
 import logo from "../text-logo.svg"
+
 const Header = () => {
+
     return (
+
+        // a header tag is returned as the parent element to keep HTML semantic
+
         <header>
-           <img id='header-logo' src={logo} alt='a light grey outline of the word "fitTrack"'></img>
+
+            {
+                //an img tag is added to display the logo
+            }
+
+           <img id='header-logo' src={logo} alt='a light grey outline of the word "fitTrack"'/>
+
         </header >
+
     );
+
 }
+
+//ES6 export statement
 export default Header;
